42652ab143b82a08dd43806d267061b6e358d103
Thomas Andersen <55490372+Thekid2002@users.noreply.github.com>
2025-10-09 13:42:32 +0200

    Works for ("all") devices

--- MEOW/MEOW/Platforms/iOS/AppDelegate.cs
+++ MEOW/MEOW/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace MEOW;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
