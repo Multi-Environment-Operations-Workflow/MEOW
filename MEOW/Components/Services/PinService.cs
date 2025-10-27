62a4c2961b2ca93f7a461d5df1ccc214313da2a1
MaksimHamko <mhamko22@student.aau.dk>
2025-10-27 10:59:23 +0100

    Some changes for the pinlist

--- MEOW/Components/Services/PinService.cs
+++ MEOW/Components/Services/PinService.cs
@@ -6,38 +6,37 @@ namespace MEOW.Components.Services;
 public class PinService : IPinService
 {
 
+    public List<PinItem> PinList = new();
+   
+    
+    // First we gotta see if there is a bluetooth connection or if they are connected to any devices    
+
     public async Task<string> ProcessedValueAsync(string input)
     {
         await Task.Delay(100);
         return $"Processed value: {input}";
     }
     
+    public async Task<string> SetPinList(PinItem pin) 
+    {
+        await Task.Delay(1000);
+        PinList.Add(pin);
+        Console.WriteLine(pin);
+        return $"Processed value: {Pin}";
+    }
+
+    public async Task<List<PinItem>> GetPinList()
+    {
+        await Task.Delay(1000);
+        return PinList;
+    }
+
     public Task<bool> SendPinMetadataAsync(PinItem pin)
     {
         throw new NotImplementedException();
     }
-
-    public Task<bool> SendPinImageAsync(PinItem pin)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<bool> SendPinChunkAsync(byte[] data, int sequenceNumber)
-    {
-        throw new NotImplementedException();
-    }
-
+    
     public Task OnPinDataReceivedAsync(byte[] data)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task OnPinImageChunkReceivedAsync(byte[] chunk, int sequenceNumber)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void AssembleCompletePin(Guid pinId)
     {
         throw new NotImplementedException();
     }
