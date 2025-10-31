10ff4b1c24fbe58c98082b346602f48b281b632d
Thomas Andersen <55490372+Thekid2002@users.noreply.github.com>
2025-10-31 10:41:10 +0100

    Fix fo interface

--- MEOW/Components/Services/IPinService.cs
+++ MEOW/Components/Services/IPinService.cs
@@ -4,9 +4,11 @@ namespace MEOW.Components.Services;
 
 public interface IPinService
 {
-    // Send basic pin info (title, text, timestamp, etc.)
     Task<bool> SendPinMetadataAsync(PinItem pin);
 
-    // Called when pin metadata is received from another device
     Task OnPinDataReceivedAsync(byte[] data);
+    
+    void AddPin(PinItem pin);
+    void RemovePin(PinItem pin);
+    List<PinItem> GetPins();
 }
