e5bc7dea97b902c45b2a3c56bef92a7ff1838223
MShadiF <113097694+MShadiF@users.noreply.github.com>
2025-11-17 23:57:13 +0100

    quick chat issues finally solved. I feel stupid

--- MEOW_BUSINESS/Services/QuickChatService.cs
+++ MEOW_BUSINESS/Services/QuickChatService.cs
@@ -1,27 +1,22 @@
 using MEOW_BUSINESS.Models;
 using MEOW_BUSINESS.Enums;
-using System.Collections.ObjectModel;
 
 namespace MEOW_BUSINESS.Services;
 
-public class QuickChatService (IMessageService messageService, IUserStateService userStateService, IErrorService errorService) : IQuickChatService
+public class QuickChatService (IMessageService messageService, 
+    IUserStateService userStateService, 
+    IErrorService errorService,
+    INotificationManagerService notificationManagerService
+    ) : IQuickChatService
 {
-    //En function til at få bytes til string
-    private static string MessageFromByte(QuickChatMessageType messageType)
-    {
-        switch(messageType)
-        {
-            case QuickChatMessageType.Help:
-                return "Oh god. Oh shit. Oh fuck. Help me right now. Jesus FUCK";
-            case QuickChatMessageType.Found:
-                return "I found the guy. I might be him.";
-            default:
-                return "Jap. He ded.";
-        }
-    }
     
-    //On quickchat function
-    //Bruges til at håndtere når man modtager en besked gennem quickchat
+    private static string MessageFromByte(QuickChatMessageType messageType)
+        => messageType switch
+        {
+            QuickChatMessageType.Help => "SOS: Need Help!",
+            QuickChatMessageType.Found => "Found The Person!",
+            _ => "Error sending quick chat message"
+        };
 
     public void SetupReceiveMessages()
     {
@@ -30,14 +25,26 @@ public class QuickChatService (IMessageService messageService, IUserStateService
     
     private void QuickChatMessageReceivedAction(MeowMessageQuickChat quickChat)
     {
-        errorService.Add(new Exception(MessageFromByte(quickChat.QMessageType) + quickChat.Sender));
+        try
+        {
+            notificationManagerService.SendNotification(
+                "QuickChat Alert",
+                $"{MessageFromByte(quickChat.QMessageType)} + {quickChat.Sender}",
+                DateTime.Now
+            );
+            errorService.Add(new Exception($"{quickChat.Longitude} {quickChat.Latitude} {MessageFromByte(quickChat.QMessageType)} {quickChat.Sender}"));
+        }
+        catch (Exception ex)
+        {
+            errorService.Add(ex);
+        }
     }
+
     
     public Task<(bool, List<Exception>)> SendMessage(QuickChat quickChat)
     {
         var meowMessage = new MeowMessageQuickChat(userStateService.GetId(), MessageService.GetMessageCount(), userStateService.GetName(),
             quickChat.Longitude, quickChat.Latitude, quickChat.Type);
-
         return messageService.SendMessage(meowMessage);
     }
 }
